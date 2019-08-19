365c252a42ff3b0b7ceb6a1eaec7430432309224
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-08-19 16:43:21 +0530

    Kivy trash

--- ki.py
+++ ki.py
@@ -3,30 +3,26 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+import time
 
-
-class MyLabel(Image):
-    text = StringProperty('')
-
-    def on_text(self, *_):
-        l = Label(text=self.text)
-        l.font_size = '30dp'  
-        l.texture_update()
-        self.texture = l.texture
-
+change=False
 class Display(App):
     transactionHash=None
-    blockNumber=None
+    label=None
     def setTransactionHash(self,transactionHash):
         self.transactionHash=transactionHash
-    def setBlockNumber(self,blockNumber):
-        self.blockNumber=blockNumber
-    
     def build(self):
-        return MyLabel(text='Transaction Hash:'+self.transactionHash+'\nBlock Number:'+self.blockNumber+'\n')
+        self.text="""
+                                     [size=50]Vote Casted![/size]
+        \n\n\n\n\n\n\n\n\n\n\n\n
+                                              [size=24]Transaction Hash[/size]
+                                              ------------------------------------------------
+        \n
+        """
+        self.label=Label(text=text+self.transactionHash,markup=True)
+        return self.label
 
 
 obj=Display()
-obj.setBlockNumber("10")
-obj.setTransactionHash("2")
+obj.setTransactionHash("0x80f34fa5e8c9047d8abf07b32fad091b9f2633b86400318aea04251b53def589")
 obj.run()
